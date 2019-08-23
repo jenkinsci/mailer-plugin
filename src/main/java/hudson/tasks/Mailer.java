08ef3862b4169499a88dd917fbee55d81efc6498
Francisco Javier Fernandez Gonzalez <fjfernandez@cloudbees.com>
2019-08-23 15:48:31 +0200

    [PRE-JENKINS-55109] Addressed feedback

--- src/main/java/hudson/tasks/Mailer.java
+++ src/main/java/hudson/tasks/Mailer.java
@@ -251,12 +251,12 @@ public class Mailer extends Notifier implements SimpleBuildStep {
          */
         private String hudsonUrl;
 
-        /** @deprecated as of 1.22, use {@link #authentication} */
+        /** @deprecated as of 1.23, use {@link #authentication} */
         @Deprecated
         private transient String smtpAuthUsername;
 
         @Deprecated
-        /** @deprecated as of 1.22, use {@link #authentication} */
+        /** @deprecated as of 1.23, use {@link #authentication} */
         private transient Secret smtpAuthPassword;
 
         private SMTPAuthentication authentication;
